92c235ccceb3df0c1901756da6285c08aafc3fbc
cedoe70 <cedoe70@gmail.com>
2025-07-30 14:39:40 -0500

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-8 bg-dark text-center text-white">
+      <p>&copy; {new Date().getFullYear()} Modern Website. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
